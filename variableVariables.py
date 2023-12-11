cdff2f515fc4bb11235e7779875fd4c28f841625
NicoAtSchwarz <nic.oblaender.23@lehre.mosbach.dhbw.de>
2023-12-11 22:59:46 +0100

    learned about string int double and their reaktion

---
+++ variableVariables.py
@@ -0,0 +1,14 @@
+Zahl= 5
+zahl2= 65
+doubleZahl = 4.0
+symbol1= "5"
+symbol2= "65"
+text1= "Hallo"
+text2= "Welt"
+
+print(Zahl + zahl2)
+print(Zahl + int(doubleZahl))
+print(Zahl + doubleZahl)
+print(symbol1 + symbol2)
+print(text1 + text2)
+print(text1 + str(Zahl))
