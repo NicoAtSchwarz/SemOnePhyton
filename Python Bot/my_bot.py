405bea07ebabeb03d4e4aa278f749cc2fd4ed079
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2023-12-20 23:49:34 +0100

    Say Hello to world if it says hello to you

--- Python Bot/my_bot.py
+++ Python Bot/my_bot.py
@@ -11,16 +11,25 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 with open('token.txt') as file:
     token = file.readline()
 
+helloGIF = 'https://tenor.com/view/hi-hello-gif-1314135106863776295'
+
 @bot.event
 async def on_startup():
     print('The bot has started!')
 
 @bot.event
-async def on_message(message):
-    if message.author == bot:
+async def on_message(message: discord.Message):
+    if message.author.bot:
         return
-    
-    # Your code to process the message here
+
     print(f'Received message from {message.author}: {message.content} ---- {message.channel}')
+
+    if message.content.lower().startswith('hello bot'):
+        print("Hello bot")
+        await message.channel.send('Hello, human!')
+
+    if "hello" or "hallo" in message.content.lower().lstrip():
+        print("Hallo oder Hello!")
+        await message.channel.send(helloGIF)
 
 bot.run(token)
