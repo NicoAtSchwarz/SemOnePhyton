292b39b18f7051568489061d88dbc4e3e5d38793
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2023-12-20 13:42:04 +0100

    working Bot - consol.log message, author, channel

--- Python Bot/my_bot.py
+++ Python Bot/my_bot.py
@@ -1,15 +1,26 @@
 import discord
 import os
+from discord.ext import commands
 
-bot = discord.Client(intents=discord.Intents.default())
 os.chdir(os.getcwd() + '\Python Bot')
+
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 with open('token.txt') as file:
     token = file.readline()
 
-intentsDC = discord.Intents.default()
-intentsDC.members = True
+@bot.event
+async def on_startup():
+    print('The bot has started!')
 
-bot = discord.Client(intents=intentsDC)
+@bot.event
+async def on_message(message):
+    if message.author == bot:
+        return
+    
+    # Your code to process the message here
+    print(f'Received message from {message.author}: {message.content} ---- {message.channel}')
 
 bot.run(token)
