afe4e55dcd293b9a2a13be57b91c5aead74f314d
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2024-01-19 02:56:03 +0100

    Fix: order slash commands

--- Python Bot/my_bot.py
+++ Python Bot/my_bot.py
@@ -42,13 +42,14 @@ mainColour = 0xa2c188
 
 
 @bot.tree.command (name="help", description="Help with everything")
-async def hello(interaction: discord.Interaction):
+async def help(interaction: discord.Interaction):
     command1 = "- `Hello [bot]` / `Hallo [bot]` - greet the bot"
     command2 = "- reacting to following words: \n>>> `dance cat` / `chipi` / `chippi` / `baby yoda` / \n`baby-yoda`"
-    commandSlash = " `help`: `bye` / `hello` / `chipi` / `dance` / `hello` / `hug` / `lol` / `love` / `say [msg you wana tell]` / `slash` / `suggest [suggestion]`"
+    commandSlash = " `help`: `bye` / `chipi` / `dance` / `hello` / `hug` / `lol` / `love` / `say [msg you wana tell]` / `slash` / `suggest [suggestion]`"
     embedHelp=discord.Embed(title="Current Commands", description=f"{command1} \n{command2}", color=mainColour)
     embedHelp.add_field(name="slash commands", value=commandSlash, inline=True)
     await interaction.response.send_message(embed=embedHelp, ephemeral=True) #privat Msg in channel
+
 
 @bot.event
 async def on_message(message: discord.Message):
@@ -84,43 +85,43 @@ async def on_message(message: discord.Message):
     if "babyyoda" in messageLowStripped or "baby-yoda" in messageLowStripped:
         await message.channel.send("Grogu my name is!"); return
 
-@bot.tree.command (name="hello", description="Say Hello!")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hey {interaction.user.display_name}! Grogu I am!", ephemeral=False) #public Msg channel
-    await interaction.channel.send(helloGIF)
-
 @bot.tree.command (name="bye", description="Bye👋")
 async def bye(interaction: discord.Interaction):
-    await interaction.response.send_message(byeGroguGIF, ephemeral=False) #public Msg channel 
-
-@bot.tree.command (name="love", description="Send love!")
-async def love(interaction: discord.Interaction):
-    await interaction.response.send_message(loveGroguGIF, ephemeral=False) #public Msg channel 
-
-@bot.tree.command (name="hug", description="Send a hug!")
-async def hug(interaction: discord.Interaction):
-    await interaction.response.send_message(hugGroguGIF, ephemeral=False) #public Msg channel 
-
-@bot.tree.command (name="dance", description="Let's dance!")
-async def dance(interaction: discord.Interaction):
-    await interaction.response.send_message(dancingDraginGIF, ephemeral=False) #public Msg channel 
+    await interaction.response.send_message(byeGroguGIF, ephemeral=False) #public Msg channel
 
 @bot.tree.command (name="chipi", description="Chipi chipi, chapa chapa...")
 async def chipi(interaction: discord.Interaction):
     await interaction.response.send_message(chipiGIF, ephemeral=False) #public Msg channel 
 
+@bot.tree.command (name="dance", description="Let's dance!")
+async def dance(interaction: discord.Interaction):
+    await interaction.response.send_message(dancingDraginGIF, ephemeral=False) #public Msg channel 
+
+@bot.tree.command (name="hello", description="Say Hello!")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hey {interaction.user.display_name}! Grogu I am!", ephemeral=False) #public Msg channel
+    await interaction.channel.send(helloGIF)
+
+@bot.tree.command (name="hug", description="Send a hug!")
+async def hug(interaction: discord.Interaction):
+    await interaction.response.send_message(hugGroguGIF, ephemeral=False) #public Msg channel 
+
 @bot.tree.command (name="lol", description="Let's dance!")
 async def lol(interaction: discord.Interaction):
     await interaction.response.send_message(ohGroguGIF, ephemeral=False) #public Msg channel 
 
-@bot.tree.command (name="slash", description="Explore slash commands")
-async def test(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hey {interaction.user.display_name}! You discovered a slash command!", ephemeral=False) #public Msg channel 
+@bot.tree.command (name="love", description="Send love!")
+async def love(interaction: discord.Interaction):
+    await interaction.response.send_message(loveGroguGIF, ephemeral=False) #public Msg channel 
 
 @bot.tree.command (name="say", description="Say me, what I should tell the others")
 @app_commands.describe (thing_to_say = "What should I say for you?")
 async def say(interaction: discord.Interaction, thing_to_say: str):
     await interaction.response.send_message (f" {interaction.user.name}: {thing_to_say}")
+
+@bot.tree.command (name="slash", description="Explore slash commands")
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hey {interaction.user.display_name}! You discovered a slash command!", ephemeral=False) #public Msg channel 
 
 @bot.tree.command (name="suggest", description="Make a suggestion")
 @app_commands.describe (suggestion = "What is your suggestion?")
